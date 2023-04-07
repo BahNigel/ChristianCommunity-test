ec53db097ba93e091334d6f2d66b58e68610a8b6
Nigel Bah <bah.nigel@yahoo.com>
2023-04-07 11:57:14 +0100

    started the foundation

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', [AuthController::class, 'showLogin'])->name('login');
+Route::get('register', [AuthController::class, 'showRegister'])->name('register');
+Route::get('chat', [ChatController::class, 'showChat'])->name('chat');
